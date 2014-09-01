3bf7a8724d83200ba52a2a5c5df7901a688648f5
Andrey Belyaev <svetomech@gmail.com>
2014-09-01 19:23:10 +0400

    Just some formatting (part 2)

--- hibernate/Program.cs
+++ hibernate/Program.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading;           // Thread
 using System.Threading.Tasks;
-using System.Diagnostics;         // Process
+//using System.Diagnostics;         // Process
 using System.Windows.Forms;       // Application
 using Microsoft.Win32;            // Registry
 
@@ -13,7 +13,7 @@ namespace hibernate
 	class Program
 	{
 		//private static string ErrorLow = "Please enter TIME argument\nUsage: hibernate <seconds>";
-		private static string ErrorMedium = "Please run as administrator";
+		private static string ErrorMedium = "Please run as administrator (one-time action)";
 
 		static int Main(string[] args)
 		{
@@ -37,54 +37,54 @@ namespace hibernate
 			}
 		}
 
-        private static bool isHibernationEnabled()
-        {
-            using (RegistryKey _reg = Registry.LocalMachine.OpenSubKey(@"System\CurrentControlSet\Control\Power\", false))
-            {
-                return Convert.ToBoolean(_reg.GetValue("HibernateEnabled", 0));
-            }
-        }
+		private static bool isHibernationEnabled()
+		{
+			using (RegistryKey _reg = Registry.LocalMachine.OpenSubKey("System\\CurrentControlSet\\Control\\Power\\", false))
+			{
+				return Convert.ToBoolean(_reg.GetValue("HibernateEnabled", 0));
+			}
+		}
 
-        private static void enableHibernation()
-        {
-            //Process.Start(@"cmd", @"/C powercfg -h on");
-            using (RegistryKey _reg = Registry.LocalMachine.OpenSubKey(@"System\CurrentControlSet\Control\Power\", true))
-            {
-                _reg.SetValue("HibernateEnabled", 1, RegistryValueKind.DWord);
-            }
-        }
+		private static void enableHibernation()
+		{
+			//Process.Start(@"cmd", @"/C powercfg -h on");
+			using (RegistryKey _reg = Registry.LocalMachine.OpenSubKey("System\\CurrentControlSet\\Control\\Power\\", true))
+			{
+				_reg.SetValue("HibernateEnabled", 1, RegistryValueKind.DWord);
+			}
+		}
 
-        private static int doHibernation(string[] args) // can be void, w/o returns and ~ (!) args
-        {
-            if (args.Length == 0)
-            {
-                Application.SetSuspendState(PowerState.Hibernate, true, true);
-                return 0;
-            }
-            else
-            {
-                uint sec;
-                bool test = uint.TryParse(args[0], out sec);
-                if (test == false)
-                {
-                    Console.WriteLine("Please enter TIME argument\nUsage: hibernate <seconds>"); // ErrorLow
+		private static int doHibernation(string[] args) // can be void, w/o returns and ~ (!) args
+		{
+			if (args.Length == 0)
+			{
+				Application.SetSuspendState(PowerState.Hibernate, true, true);
+				return 0;
+			}
+			else
+			{
+				uint sec;
+				bool test = uint.TryParse(args[0], out sec);
+				if (test == false)
+				{
+					Console.WriteLine("Please enter TIME argument\nUsage: hibernate <seconds>"); // ErrorLow
                     return 1;
-                }
+				}
 
-                Thread.Sleep(Convert.ToInt32(sec * 1000));
+				Thread.Sleep(Convert.ToInt32(sec * 1000));
 
-                Application.SetSuspendState(PowerState.Hibernate, true, true);
-                return 0;
-            }
-        }
+				Application.SetSuspendState(PowerState.Hibernate, true, true);
+				return 0;
+			}
+		}
 
-        private static bool isAppElevated()
-        {
-            RegistryKey _reg;
-            try { _reg = Registry.LocalMachine.OpenSubKey(@"Software\", true); }
-            catch { return false; }
-            _reg.Close();
-            return true;
-        }
-    }
+		private static bool isAppElevated()
+		{
+			RegistryKey _reg;
+			try { _reg = Registry.LocalMachine.OpenSubKey("Software\\", true); }
+			catch { return false; }
+			_reg.Close();
+			return true;
+		}
+	}
 }
